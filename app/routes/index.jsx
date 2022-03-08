e1d4500f3b6c41d25c681da013622b594f8dcfb5
Francis Agulto <francisagulto@MacBook-Pro-2.attlocal.net>
2022-03-08 10:49:28 -0600

    added index file

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,31 +1,33 @@
+import { Link, useLoaderData } from 'remix';
+import slugify from 'slugify';
+import NetlifyGraph from '../../netlify/functions/netlifyGraph';
+
+export const loader = async () => {
+  const { data } = await NetlifyGraph.fetchIssues(
+    { owner: 'Fran-A-Dev', name: 'netlifygraph-ghub' },
+    { accessToken: process.env.ONEGRAPH_AUTHLIFY_TOKEN }
+  );
+
+  return data.gitHub.repository.issues.edges.map(({ node }) => {
+    return {
+      ...node,
+      slug: slugify(node.title).toLowerCase()
+    };
+  });
+};
+
 export default function Index() {
+  let data = useLoaderData();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+    <div>
+      <h1>Issues</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {data.map((item) => (
+          <li key={item.id}>
+            <Link to={item.slug}>{item.title}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
---
+++ netlify/functions/netlifyGraph/index.d.ts
@@ -0,0 +1,130 @@
+// GENERATED VIA NETLIFY AUTOMATED DEV TOOLS, EDIT WITH CAUTION!
+
+export type NetlifyGraphFunctionOptions = {
+  /**
+   * The accessToken to use for the request
+   */
+  accessToken?: string;
+  /**
+   * The siteId to use for the request
+   * @default process.env.SITE_ID
+   */
+  siteId?: string;
+};
+
+export type WebhookEvent = {
+  body: string;
+  headers: Record<string, string | null | undefined>;
+};
+
+export type GraphQLError = {
+  path: Array<string | number>;
+  message: string;
+  extensions: Record<string, unknown>;
+};
+
+export type IssuesInput = {
+  /**
+   * The login field of a user or organization
+   */
+  owner?: string;
+  /**
+   * The name of the repository
+   */
+  name?: string;
+};
+
+export type Issues = {
+  /**
+   * Any data from the function will be returned here
+   */
+  data: {
+    gitHub: {
+      /**
+       * Lookup a given repository by the owner and repository name.
+       */
+      repository: {
+        /**
+         * A list of issues that have been opened in the repository.
+         */
+        issues: {
+          /**
+           * A list of edges.
+           */
+          edges: Array<{
+            /**
+             * The item at the end of the edge.
+             */
+            node: {
+              id: string;
+              /**
+               * Identifies the body of the issue.
+               */
+              body: string;
+              /**
+               * Identifies the issue title.
+               */
+              title: string;
+              /**
+               * Identifies the date and time when the object was created.
+               */
+              createdAt: unknown;
+              /**
+               * The HTTP URL for this issue
+               */
+              url: string;
+              /**
+               * A list of comments associated with the Issue.
+               */
+              comments: {
+                /**
+                 * A list of nodes.
+                 */
+                nodes: Array<{
+                  /**
+                   * The body as Markdown.
+                   */
+                  body: string;
+                  /**
+                   * Identifies the date and time when the object was created.
+                   */
+                  createdAt: unknown;
+                  id: string;
+                  /**
+                   * The actor who authored the comment.
+                   */
+                  author: {
+                    /**
+                     * A URL pointing to the actor's public avatar.
+                     */
+                    avatarUrl: string;
+                    /**
+                     * The username of the actor.
+                     */
+                    login: string;
+                  };
+                  /**
+                   * The HTTP URL for this issue comment
+                   */
+                  url: string;
+                }>;
+              };
+            };
+          }>;
+        };
+      };
+    };
+  };
+  /**
+   * Any errors from the function will be returned here
+   */
+  errors: Array<GraphQLError>;
+};
+
+/**
+ * An example query to start with.
+ */
+export function fetchIssues(
+  variables: IssuesInput,
+  options?: NetlifyGraphFunctionOptions
+): Promise<Issues>;
